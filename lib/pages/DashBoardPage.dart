63bd00f87b8255b91f814621eb23c8729f0b3769
Ploope <maxime.le-gall@epitech.eu>
2021-04-18 20:59:52 +0200

    pulled everything

--- lib/pages/DashBoardPage.dart
+++ lib/pages/DashBoardPage.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:note_illustrator/widgets/BottomAppBar.dart';
+import 'package:flappy_search_bar/flappy_search_bar.dart';
+import '../widgets/Notes.dart';
 
 class DashBoardPage extends StatefulWidget {
   @override
@@ -7,10 +9,27 @@ class DashBoardPage extends StatefulWidget {
 }
 
 class _DashBoardPageState extends State<DashBoardPage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        bottomNavigationBar: BottomAppBarWidget(),
-        body: Center(child: Text('Dashboard Page')));
-  }
+	@override
+	Widget build(BuildContext context) {
+		return Scaffold(
+			bottomNavigationBar: BottomAppBarWidget(),
+			body: Center(child: NotesPage()));
+		// 	body: Center(
+		// 		child: Column(
+		// 			mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+		// 			children: [
+		// 				SearchBar(),
+		// 				NotesPage()
+		// 			// Expanded(
+		// 			// child: Padding(
+		// 			//   padding: const EdgeInsets.all(20.0),
+		// 			//   child: SearchBar(),)
+		// 			// child: NotesPage()
+		// 			// )
+		// 			]
+		// 			// )
+		// 			//  NotePages()
+		// 			)),
+		// );
+	}
 }
